f3c29238f694e04a6f3b53b406c2d5d11ef621f1
Elronia <zar.catherine@gmail.com>
2021-05-01 23:20:05 -0400

    adding all the necessary variables and EventListener

---
+++ script.js
@@ -0,0 +1,13 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
