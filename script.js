9e444601a19c36b5f91b24584de53fe6312c84e8
Elronia <zar.catherine@gmail.com>
2021-05-02 23:46:50 -0400

    adding EventListeners for Stop and Start buttons, added a filter by language for Dropdown list

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
     voices = this.getVoices();
     voicesDropdown.innerHTML = voices
+        .filter(voice => voice.lang.includes('ru'))
         .map(voice => `<option value="${voice.name}">${voice.name} (${voice.name} (${voice.lang})</option>)`)
         .join('');
 }
@@ -26,5 +27,14 @@ function toggle(startOver = true) {
     
 }
 
+function setOption() {
+    console.log(this.name, this.value);
+    msg[this.name] = this.value;
+    toggle();
+}
+
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
